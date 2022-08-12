4a7675200ce5fa408b359991d8b25a3ae9d4692a
Jung <wjdrbcjf97@naver.com>
2022-08-13 01:04:02 +0900

    ui파일 업로드

---
+++ ui/out.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'outgQegeo.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(658, 300)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.pushButton_2 = QPushButton(self.centralwidget)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+        self.pushButton_2.setGeometry(QRect(540, 210, 93, 28))
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(30, 50, 201, 16))
+        self.pushButton = QPushButton(self.centralwidget)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(440, 210, 93, 28))
+        self.textEdit = QTextEdit(self.centralwidget)
+        self.textEdit.setObjectName(u"textEdit")
+        self.textEdit.setGeometry(QRect(30, 20, 201, 31))
+        self.label_3 = QLabel(self.centralwidget)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setGeometry(QRect(250, 40, 371, 141))
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 658, 26))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"\ucde8\uc18c", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"\ucc28\ub7c9\ubc88\ud638\uc785\ub825", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"\uc785\ub825", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"<\uc0ac\uc6a9\uc548\ub0b4>\n"
+"\n"
+" \ucd9c\ucc28\ucc28\ub7c9\uc744 \uc704\ud55c \ud654\uba74\uc785\ub2c8\ub2e4. \n"
+"\n"
+" 1. \ucc28\ub7c9\ubc88\ud638\ub97c \uc785\ub825\ud574\uc8fc\uc138\uc694. \n"
+" 2. \ucc28\ub7c9\uc5d0 \ubb38\uc81c\uac00 \uc0dd\uacbc\ub2e4\uba74 \uad00\ub9ac\uc790\uc5d0\uac8c \ubb38\uc758\ud558\uc138\uc694", None))
+    # retranslateUi
+
